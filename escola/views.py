402013147796562099982883a252ced7fc9efcd5
Reni-amorim <reni.425@gmail.com>
2025-06-06 00:50:19 -0300

    This commit covers learning about Django REST framework ViewSets as an alternative to traditional Views for more streamlined CRUD operations, and implementing automatic URL routing using routers to create endpoints for the API root, Students, and Courses.

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
 
-def estudantes(request):
-    if request.method == "GET":
-        estudante = {
-            'id': 1,
-            'nome': 'João'
-        }
-        return JsonResponse(estudante)
+class EstudantesViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os estudantes"""
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from escola.views import estudantes
+from django.urls import path,include
+from escola.views import EstudantesViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudantes', EstudantesViewSet, basename='Estudantes')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estudantes/', estudantes)
+    path('', include(router.urls))
 ]
